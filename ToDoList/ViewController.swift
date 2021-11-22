6b63d35ccb0dab2df320b7639dc247cfe07d9211
hoon <skawlgns@kakao.com>
2021-11-22 10:32:51 +0900

    Alert

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapEditBtn(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        
+       let alert = UIAlertController(title: "오늘 뭐하지?", message: nil, preferredStyle: .alert)
+       let registAlert = UIAlertAction(title: "등록", style: .default, handler: {_ in})
+       let cancelAlert = UIAlertAction(title: "취소", style: .default, handler: nil)
+        alert.addAction(cancelAlert)
+        alert.addAction(registAlert)
+        alert.addTextField(configurationHandler: {textField in
+            textField.placeholder = "할 일을 입력해주세요!"
+        })
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
 
