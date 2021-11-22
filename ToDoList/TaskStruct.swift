89177fe8244338728462f889131e6fa2937b9b3d
hoon <skawlgns@kakao.com>
2021-11-22 11:08:59 +0900

    register_todo

---
+++ ToDoList/TaskStruct.swift
@@ -0,0 +1,13 @@
+//
+//  TaskStruct.swift
+//  ToDoList
+//
+//  Created by 남지훈 on 2021/11/22.
+//
+
+import Foundation
+
+struct TaskStruct {
+    var todo: String
+    var complete: Bool
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -11,8 +11,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    
+    var taskStruct = [TaskStruct]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
@@ -22,7 +27,12 @@ class ViewController: UIViewController {
     @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
         
        let alert = UIAlertController(title: "오늘 뭐하지?", message: nil, preferredStyle: .alert)
-       let registAlert = UIAlertAction(title: "등록", style: .default, handler: {_ in})
+        let registAlert = UIAlertAction(title: "등록", style: .default, handler: {[weak self] _ in
+        guard let textFieldValue = alert.textFields?[0].text else { return }
+        let task = TaskStruct(todo: textFieldValue, complete: false)
+        self?.taskStruct.append(task)
+        self?.tableView.reloadData()
+       })
        let cancelAlert = UIAlertAction(title: "취소", style: .default, handler: nil)
         alert.addAction(cancelAlert)
         alert.addAction(registAlert)
@@ -34,3 +44,15 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.taskStruct.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = self.taskStruct[indexPath.row]
+        cell.textLabel?.text = task.todo
+        return cell
+    }
+}
